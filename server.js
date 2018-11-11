189589f37186f0001453f8119c337d08421bb6bd
Earle-Poole <eepoole4@gmail.com>
2018-11-10 20:17:30 -0600

    My first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profileId = require('./controllers/profileId');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        password : 'abcd1234',
+        database : 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
+
+app.get('/profile/:id', (req, res) => {profileId.handleProfileId(req, res, db)} )
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)} )
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)} )
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`)
+})
+
+console.log(PORT);
