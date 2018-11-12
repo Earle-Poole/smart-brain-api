601d49f523d070d84943809cf162378d121296ad
Earle-Poole <eepoole4@gmail.com>
2018-11-11 21:26:19 -0600

    update postgres database info

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-silhouetted-76869',
         user : 'postgres',
         password : 'abcd1234',
         database : 'smart-brain'
