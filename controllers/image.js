49b96bf6c5965b0673a8b952191e3fde96c28c3c
earle-poole <eepoole4@gmail.com>
2018-11-26 20:10:21 -0600

    trying faceFound if statement on entries again

--- controllers/image.js
+++ controllers/image.js
@@ -4,25 +4,30 @@ const app = new Clarifai.App({
     apiKey: 'a071956ee8854fe2b62e11dd64ff038c'
   });
 
+let faceFound = {};
+
 const handleApiCall = (req, res) => {
 app.models.predict(
     Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
-        res.json(data);
+        faceFound = data;
+        return res.json(data);
     })
     .catch(err => res.status(400).json('unable to work with api'))
 }
 
-const handleImage = (req, res, db) => {
-    const { id } = req.body;
-    db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
+if(faceFound) {
+    const handleImage = (req, res, db) => {
+        const { id } = req.body;
+        db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+    }
 }
 
 module.exports = {
