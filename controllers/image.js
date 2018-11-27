50aae2132940c7d97bc11e79f756bb016d0224fd
earle-poole <eepoole4@gmail.com>
2018-11-26 20:34:10 -0600

    more if statement testing

--- controllers/image.js
+++ controllers/image.js
@@ -10,7 +10,7 @@ const handleApiCall = (req, res) => {
 app.models.predict(
     Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
-        faceFound = data;
+        faceFound = data.outputs[0].data.regions;
         res.json(data);
     })
     .catch(err => res.status(400).json('unable to work with api'))
