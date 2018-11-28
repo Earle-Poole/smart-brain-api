cf8909dea7fc4653ecab90b71f99e444f617948e
Earle-Poole <eepoole4@gmail.com>
2018-11-28 13:32:06 -0600

    Tweaks to server

--- server.js
+++ server.js
@@ -29,11 +29,8 @@ app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)} )
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
 app.get('/profile/:id', (req, res) => {profileId.handleProfileId(req, res, db)} )
 app.put('/image', (req, res) => {image.handleImage(req, res, db)} )
-app.post('/imageurl', (req, res) => (() => {
-    image.handleApiCall(req, res)
-    image.handleGeneralModelApiCall(req, res)
-    }
-))
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)} )
+app.post('/generalmodelurl', (req, res) => {image.handleGeneralModelApiCall(req, res)} )
 
 const PORT = process.env.PORT;
 app.listen(PORT || 3000, () => {
